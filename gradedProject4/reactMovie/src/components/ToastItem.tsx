8dd1ab1ebe9de0a66122484e95d3c9f4871bf5f1
vyankteshg123 <vyankteshduttmishra@gmail.com>
2023-03-12 21:09:12 +0530

    vyankteshMishra_ReactJS_Project

---
+++ gradedProject4/reactMovie/src/components/ToastItem.tsx
@@ -0,0 +1,25 @@
+import { Toast } from "react-bootstrap";
+import { useState } from "react";
+
+type Props = {
+  toastString: string;
+  toastBgType: string;
+};
+
+const ToastItem = ({ toastString, toastBgType }: Props) => {
+  const [show, setShow] = useState(true);
+
+  return (
+    <Toast
+      onClose={() => setShow(false)}
+      show={show}
+      delay={5000}
+      autohide
+      bg={toastBgType}
+    >
+      <Toast.Body>{toastString}</Toast.Body>
+    </Toast>
+  );
+};
+
+export default ToastItem;
